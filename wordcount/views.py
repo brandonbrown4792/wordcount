10d8f088b37442b2c1712eb0126db703ed76660e
Brandon Brown <hoobie4792@yahoo.com>
2020-10-05 16:43:46 -0400

    More learning

--- wordcount/views.py
+++ wordcount/views.py
@@ -4,6 +4,9 @@ import operator
 def homepage(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
 
+def about(request):
+    return render(request, 'about.html')
+
 def count(request):
     fulltext = request.GET['fulltext']
 
@@ -20,6 +23,3 @@ def count(request):
     sortedWords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
 
     return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedWords': sortedWords})
-
-def about(request):
-    return render(request, 'about.html')
